bc3ec08b496a4f39241d7a2d4c49939831d2f757
Nicol Spies <nicol.spies@gmail.com>
2017-03-09 10:37:26 +0200

    Update funcs2.lua

--- funcs2.lua
+++ funcs2.lua
@@ -1,19 +1,5 @@
 local mod = {MOD_NAME = "mod"}
 
-function mod:BatRead()
-    if rtcmem.read32(22)==1 then print(node.heap()..":"..rtctime.get().." --BatRead--") end
-    i2c.start(0)
-    if i2c.address(0, 0x48, i2c.RECEIVER) then
-        local b = i2c.read(0, 2) -- read two bytes
-        i2c.stop(0)
-        cfg0.Bat = string.format("%.3f",(b:byte(1) * 256 + b:byte(2))*1.279e-4)
-        return self:WiFiConnect()
-    else
-        cfg0.Bat = 99
-        return self:WiFiConnect()
-    end    
-end
-
 function mod:OtaChk()
     if rtcmem.read32(22)==1 then print(node.heap()..":"..rtctime.get().." --OtaChk--") end
     
